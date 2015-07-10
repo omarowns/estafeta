9fe969fd5904303273d28538715b9e3ba226cfb8
Omar Garcia <isc.omargarcia@gmail.com>
2015-07-10 00:19:35 -0500

    Adding parse functionality

--- spec/estafeta_spec.rb
+++ spec/estafeta_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.hook_into :webmock
+end
+
+module EstafetaVcr
+  def self.api_post
+    VCR.use_cassette('estafeta_api_post_successful') do
+      yield
+    end
+  end
+end
 
 describe Estafeta do
 
@@ -9,7 +23,6 @@ describe Estafeta do
   describe 'API' do
 
     let(:numero_guia_exitoso) { 6659999999061710015592 }
-    let(:codigo_rastreo_exitoso) { 1749215347 }
 
     it 'responds to an endpoint variable' do
       expect(Estafeta::API::ENDPOINT).not_to be nil
@@ -26,17 +39,47 @@ describe Estafeta do
       expect(api.query[:guias]).to eq numero_guia_exitoso
     end
 
+    it 'should be more friendly to call' do
+      pending "Make it so I dont have to instantiate a class and stuff"
+    end
+
     describe 'making HTTP requests' do
 
       let(:api) { Estafeta::API.new(guia_numero: numero_guia_exitoso) }
 
       it 'can make a HTTP post to endpoint' do
-        expect(api.post).not_to be nil
+        EstafetaVcr.api_post do
+          expect(api.post).not_to be nil
+        end
       end
 
       it 'saves the resulting POST to an instance variable' do
-        api.post
-        expect(api.page).not_to be nil
+        EstafetaVcr.api_post do
+          api.post
+          expect(api.page).not_to be nil
+        end
+      end
+    end
+
+    describe 'retrieving a doc page' do
+
+      let(:api) { Estafeta::API.new(guia_numero: numero_guia_exitoso) }
+
+      before do
+        EstafetaVcr.api_post { api.post }
+        api.retrieve_page
+      end
+
+      it 'has a doc instance variable' do
+        expect(api.doc).not_to be nil
+      end
+
+      it 'can parse the web page' do
+        expect(api.doc.css('html')).not_to be nil
+      end
+
+      it 'parses the web page' do
+        api.parse
       end
     end
   end
